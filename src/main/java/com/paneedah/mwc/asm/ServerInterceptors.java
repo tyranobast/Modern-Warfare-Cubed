7f99c5676237c380e1fb8a4a15ae14ceb7bfc36f
bat_bast <batbast.gaming@gmail.com>
2025-08-22 03:41:16 +0200

    fixed knockback again

--- src/main/java/com/paneedah/mwc/asm/ServerInterceptors.java
+++ src/main/java/com/paneedah/mwc/asm/ServerInterceptors.java
@@ -7,13 +7,6 @@ import net.minecraft.util.EntityDamageSourceIndirect;
 public class ServerInterceptors {
 
     public static float getKnockback(DamageSource source) {
-        float strength = 0.4f;
-        if (source instanceof EntityDamageSourceIndirect) {
-            EntityDamageSourceIndirect indirectSource = (EntityDamageSourceIndirect) source;
-            if (indirectSource.getImmediateSource() instanceof WeaponSpawnEntity) {
-                strength = 0;
-            }
-        }
-        return strength;
+        return source instanceof WeaponSpawnEntity.ProjectileDamageSource ? 0 : 0.4f;
     }
 }
